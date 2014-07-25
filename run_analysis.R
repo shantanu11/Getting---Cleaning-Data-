9c9126e8f11875412bb2b73a8e62043889c50dc8
Shantanu <shan123pra@gmail.com>
2014-07-25 23:22:02 +0530

    Final Code

--- run_analysis.R
+++ run_analysis.R
@@ -17,18 +17,18 @@ require("data.table")
 require("reshape2")
 
 # Load: activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/activity_labels.txt")[,2]
 
 # Load: data column names
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/features.txt")[,2]
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
 
 # Load and process X_test & y_test data.
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/test/subject_test.txt")
 
 names(X_test) = features
 
@@ -44,10 +44,10 @@ names(subject_test) = "subject"
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
 # Load and process X_train & y_train data.
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+X_train <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/train/y_train.txt")
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_train <- read.table("F:/IMT/Courses/R/CleaningData/ProjectCode/UCI HAR Dataset/train/subject_train.txt")
 
 names(X_train) = features
 
@@ -72,4 +72,4 @@ melt_data = melt(data, id = id_labels, measure.vars = data_labels)
 # Apply mean function to dataset using dcast function
 tidy_data = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
-write.table(tidy_data, file = "./tidy_data.txt")
+write.table(tidy_data, file = "F:/IMT/Courses/R/CleaningData/ProjectCode/tidy_data.txt")
